c2ee7340998747878bfb0fc0cb43d6ff3d304550
Logaridexus <domagoj.sevcenko3@gmail.com>
2026-06-04 18:12:45 +0200

    Create vjezba1.php

---
+++ Vježba 1/vjezba1.php
@@ -0,0 +1,17 @@
+<?php
+$naslov = "PHP";
+$autor = "Domagoj Sevcenko";
+echo "<h1>$naslov</h1>";
+echo "<p>Ovu stranicu izradio je <strong>$autor</strong>.</p>";
+echo '<a href="https://www.index.hr" target="_blank">Posjeti INDEX.HR</a>';
+?>
+
+<!DOCTYPE html>
+<html lang="hr">
+<head>
+    <meta charset="UTF-8">
+    <title>Vjezba 1</title>
+</head>
+<body>
+</body>
+</html>
